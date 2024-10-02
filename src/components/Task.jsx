486452eb5b6a1f76bad41f4fa779d70e048a8b55
diacaram <diacrami@espol.edu.ec>
2024-10-02 13:33:13 -0500

    Buttons change

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,64 +4,83 @@ import { taskAPI } from "../api/tasksAPI";
 import Swal from "sweetalert2";
 
 
-const Task = ({task, setTask, setModal}) => {
+const Task = ({ task, setTask, setModal }) => {
 
-    const {taskname, state} = task;
+  const { taskname, state } = task;
 
-    const editModal = () => {
-        setModal(true);
-        setTask(task);
+  const editModal = () => {
+    setModal(true);
+    setTask(task);
+  }
+
+  const taskUpdate = async (e) => {
+
+    //console.log(e.target.checked);
+    try {
+      const { taskid, taskname, state, isActive } = task;
+      let estado;
+      if (e.target.checked) { //state === 'T'
+        estado = 'C';
+      } else {
+        estado = 'T';
+      }
+      const objetoTarea = {
+        taskname,
+        state: estado,
+        isActive
+      }
+
+
+      await taskAPI.put(`updatetask/${taskid}`, objetoTarea)
+        .then(() => {
+          setTask(task);
+        });
+    } catch (error) {
+      Swal.fire({
+        title: "There has been an error",
+        text: error,
+        icon: "error",
+        confirmButtonText: "OK",
+      });
     }
+  };
 
-    const taskUpdate = async () => {
+  const taskDelete = async () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then(async (result) => {
+      if (result.isConfirmed) {
 
         try {
-            const {taskid, taskname, state, isActive} = task;
-            let estado;
-            if (state === 'T') {
-                estado = 'C';
-            } else {
-                estado = 'T';
-            }
-            const objetoTarea = {
-                taskname, 
-                state: estado,
-                isActive
-            }
-    
-          
-          await taskAPI.put(`updatetask/${taskid}`, objetoTarea)
-          .then(() => {
-            setTask(task);
-          });
-        } catch (error) {
-          Swal.fire({
-            title: "There has been an error",
-            text: error,
-            icon: "error",
-            confirmButtonText: "OK",
-          });
-        }
-      };
-
-    const taskDelete = async () => {
-        try{
-          const {taskid, taskname, state, isActive} = task;
+          const { taskid, taskname, state, isActive } = task;
           let activo;
-          if(isActive===true){
-            activo=false;
-          }else{
-            activo=true;
+          if (isActive === true) {
+            activo = false;
+          } else {
+            activo = true;
           }
           const objetoTarea = {
-            taskname, 
+            taskname,
             state,
-            isActive:activo
+            isActive: activo
           }
           await taskAPI.put(`updatetask/${taskid}`, objetoTarea)
-          .then(() => {
-            setTask(task);
-          });
+            .then(() => {
+              setTask(task);
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your task has been deleted.",
+                icon: "success",
+                confirmButtonColor: "#3085d6",
+              });
+            });
+
         } catch (error) {
           Swal.fire({
             title: "There has been an error",
@@ -70,69 +89,77 @@ const Task = ({task, setTask, setModal}) => {
             confirmButtonText: "OK",
           });
         }
-      
+
       }
-    
+    });
+
+
+  }
+
   return (
     <>
-          <div className="w-96 h-full flex flex-row justify-center mt-1 mb-1">
-              <div className={"border-2 border-white bg-white w-10/12 h-full justify-end p-2 " + (state == 'C' ? 'opacity-60' : 'opacity-100')}>
-                  <label
-                      className={"bg-white w-full h-full text-center " + (state == 'C' ? 'line-through' : 'no-underline')}
-                  >
-                      {taskname}
-                  </label>
-              </div>
+      <div className="w-[400px] h-full flex flex-row justify-center mt-1 mb-1">
+        <div className={"border-2 border-white bg-white w-9/12 h-full justify-end p-2 " + (state == 'C' ? 'opacity-60' : 'opacity-100')}>
+          <label
+            className={"bg-white w-full h-full text-center " + (state == 'C' ? 'line-through' : 'no-underline')}
+          >
+            {taskname}
+          </label>
+        </div>
 
-              <div className="border-white bg-white flex">
-                  <button
-                      className="border-2 w-10 h-10 flex justify-end bg-white border-white p-2"
-                      onClick={() => { taskUpdate(); }}
-                  >
-                      {
-                          task.state == 'T'
-                              ?
-                              <img
-                                  src="./assets/uncheck_.jpg"
-                                  className="h-full w-30"
-                                  alt="add"
-                              />
-                              :
-                              <img
-                                  src="./assets/check_.jpg"
-                                  className="h-full w-30"
-                                  alt="add"
-                              />
+        <div className="w-3/12 border-white bg-white grid grid-cols-3 place-items-center">
+          <div className="bg-white w-5 h-full flex items-center">
 
-
-                      }
-
-                  </button>
-                  <button
-                      className="border-2 w-10 h-10 flex justify-end bg-white border-white p-2"
-                      onClick={() => editModal()}
-                  >
-                      <img
-                          src="./assets/edit_.jpg"
-                          className="h-full w-30"
-                          alt="edit"
-                      />
-
-                  </button>
-
-                  <button
-                      className="border-2 w-10 h-10 flex justify-end bg-white border-white p-2"
-                      onClick={() => taskDelete()}
-                  >
-                      <img
-                          src="./assets/delete_.png"
-                          className="h-full w-30"
-                          alt="delete"
-                      />
-
-                  </button>
-              </div>
+            <input className="h-5 w-5 hover:cursor-pointer hover:bg-slate-300 accent-black" type="checkbox" onChange={taskUpdate} checked={task.state == 'T' ? false : true} />
           </div>
+          {/* <button
+            className="border-2 w-10 h-10 flex justify-end bg-white border-white p-2 hover:bg-gray-400"
+            onClick={() => { taskUpdate(); }}
+          >
+            {
+              task.state == 'T'
+                ?
+                <img
+                  src="./assets/uncheck_.jpg"
+                  className="h-full w-30"
+                  alt="uncheck"
+                />
+                :
+                <img
+                  src="./assets/check_.jpg"
+                  className="h-full w-30"
+                  alt="check"
+                />
+
+
+            }
+
+          </button> */}
+          <button
+            className="h-full w-5 bg-white"
+            onClick={() => editModal()}
+          >
+            <img
+              src="./assets/edit.svg"
+              className=" hover:cursor-pointer hover:bg-slate-300"
+              alt="edit"
+            />
+
+          </button>
+
+          <button
+            className="h-full w-5 bg-white"
+            onClick={() => taskDelete()}
+          >
+            <img
+              src="./assets/delete_.png"
+              className=" hover:cursor-pointer hover:bg-slate-300"
+              alt="delete"
+            />
+
+          </button>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -5,17 +5,17 @@ import axios from "axios";
 import Swal from "sweetalert2";
 import { taskAPI } from "../api/tasksAPI";
 
-const TaskForm = ({setTask, taskList, setError}) => {
+const TaskForm = ({ setTask, taskList, setError }) => {
 
     const [taskname, setTaskName] = useState('');
     const [state, setState] = useState('T');
-    
+
 
     useEffect(() => {
-          
-      return () => {
-        
-      }
+
+        return () => {
+
+        }
     }, [taskList])
 
     const handleSubmit = async (e) => {
@@ -32,11 +32,16 @@ const TaskForm = ({setTask, taskList, setError}) => {
                 taskname,
                 state
             }
-            
+
 
             await taskAPI.post("/createtask", taskObject);
             setTask(taskObject)
             setTaskName('');
+            Swal.fire({
+                title: "Task added",
+                icon: "success",
+                confirmButtonText: "OK",
+            });
 
         } catch (error) {
             Swal.fire({
@@ -45,43 +50,46 @@ const TaskForm = ({setTask, taskList, setError}) => {
                 icon: "error",
                 confirmButtonText: "OK",
             });
-        }        
-    }   
+        }
+    }
 
-  return (
-    
-    <>
-          <div>
-              <form
-                  id="form"
-                  onSubmit={handleSubmit}
-                  action=""
+    return (
 
-              >
-                  <div className="md:w-full lg:w-full mb-5 flex flex-row rounded-tl-md rounded-bl-md">
-                      <input
-                          id="task"
-                          type="text"
-                          placeholder="Enter Task"
-                          className="border-2 w-4/5 h-12 p-2 border-white rounded-tl-md rounded-bl-md"
-                          value={taskname}
-                          onChange={(e) => setTaskName(e.target.value)}
-                      />
-                      <button
-                          type="submit"
-                          className="border-2 w-1/5 h-12 flex justify-end bg-white border-white rounded-tr-md rounded-br-md"
-                      >
-                          <img
-                              src="./assets/add_.png"
-                              className="h-8 w-8 mt-1 mr-2"
-                              alt="add"
-                          />
-                      </button>
-                  </div>
-              </form>
-          </div>    
-    </>
-  )
+        <>
+            <div>
+                <form
+                    id="form"
+                    onSubmit={handleSubmit}
+                    action=""
+
+                >
+                    <div className="md:w-full lg:w-full mb-5 flex flex-row rounded-tl-md rounded-bl-md">
+                        <input
+                            id="task"
+                            type="text"
+                            placeholder="Enter Task"
+                            className="border-2 w-4/5 h-12 p-2 border-white rounded-tl-md rounded-bl-md"
+                            value={taskname}
+                            onChange={(e) => setTaskName(e.target.value)}
+                        />
+                        <button
+                            type="submit"
+                            className="relative w-1/5 h-12 flex justify-end bg-white rounded-tr-md rounded-br-md"
+                        >
+                            <div className="h-full flex items-center justify-end rounded-tr-md rounded-br-md hover:bg-gray-400">
+
+                                <img
+                                    src="./assets/add_.png"
+                                    className="h-6 w-6 ml-2 mr-2"
+                                    alt="add"
+                                />
+                            </div>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
 }
 
 export default TaskForm
